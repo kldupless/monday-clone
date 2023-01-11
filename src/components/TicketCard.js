e016a0c14450e28236e7a31b92f9424a56d12afd
Krysia Sherburne <43474151+kldupless@users.noreply.github.com>
2023-01-11 22:14:31 +0000

    Create dummy data Map data to dashboard Format ticket card

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,8 +1,24 @@
-const TicketCard = () => {
+import { Link } from 'react-router-dom'
+import classnames from 'classnames'
+import AvatarDisplay from './AvatarDisplay'
+import StatusDisplay from './StatusDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import PriorityDisplay from './PriorityDisplay'
+import DeleteBlock from './DeleteBlock'
+
+const TicketCard = ({ color, ticket }) => {
 
     return (
-        <div>
-            Ticket card
+        <div className="flex w-full">
+            <Link to={`/ticket/${ticket.documentId}`} id="link" className="flex w-full">
+                <div className={classnames("w-4 m-1 p-2", {color})}></div>
+                <h3 className="m-1 p-5 w-full flex items-center bg-gray-400">{ticket.title}</h3>
+                <AvatarDisplay ticket={ticket}/>
+                <StatusDisplay />
+                <ProgressDisplay />
+                <PriorityDisplay />
+            </Link>
+            <DeleteBlock/>
         </div>
     )
 }
