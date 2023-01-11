f922c9c7e55516d0099e6a6ad829c12c3aeb827e
Krysia Sherburne <43474151+kldupless@users.noreply.github.com>
2023-01-11 19:33:49 +0000

    Add routes for app pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import TicketPage from './pages/TicketPage'
+
 const App = () => {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/ticket' element={<TicketPage/>}/>
+          <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
