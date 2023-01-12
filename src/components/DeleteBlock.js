18f50d63f4c9e666a79a378fddac6682ba4d7638
Krysia Sherburne <43474151+kldupless@users.noreply.github.com>
2023-01-12 17:14:13 +0000

    Style delete block

--- src/components/DeleteBlock.js
+++ src/components/DeleteBlock.js
@@ -1,8 +1,15 @@
+import { ReactComponent as Delete } from '../images/icon-close.svg'
 const DeleteBlock = () => {
 
+    const deleteTicket = () => {
+        console.log('Ticket deleted')
+    }
+
     return (
-        <div>
-            DeleteBlock
+        <div className="m-1 p-3 flex items-center justify-center bg-gray-100">
+            <div className="w-5 h-5 flex items-center justify-center" onClick={deleteTicket}>
+                <Delete className="fill-red-500"/>
+            </div>
         </div>
     )
 }
